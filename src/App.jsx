e45addba0ba492472a2118187504bf0cc4a4f4d7
Arnav-Bhatnagar <arnavbhatnagar2004@gmail.com>
2025-09-24 19:12:43 +0530

    chatbot

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import ChatBot from './coponents/chatbot'
+import './App.css'
+
+function App() {
+  return (
+    <div className="app-container">
+      
+      <ChatBot />
+    </div>
+  )
+}
+
+export default App
